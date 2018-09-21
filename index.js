5da2e8513ed31e7e9b1831d621340e5dc6582d76
Preeternal <preeternal@ya.ru>
2018-09-21 13:15:00 +0300

    separate store

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// import libraries
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-import {
-  AppRegistry,
-  // Text,
-  ScrollView,
-  //  StyleSheet
-} from 'react-native';
+import { AppRegistry, ScrollView } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app';
-import reducers from './src/reducers';
+import store from './src/store';
+// import reducers from './src/reducers';
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-// export const subscribe = store.subscribe(() => {
-//   return store.getState();
-// });
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
 
 const Start = () => (
   <Provider store={store}>
     <ScrollView>
       <App />
-      {/* <Text> {'\n'} </Text> */}
     </ScrollView>
   </Provider>
 );
