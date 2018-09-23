fd920f65df9fdb37c703c46c4094c8f4fc5ca834
Preeternal <preeternal@ya.ru>
2018-09-23 12:15:32 +0300

    android

--- android/app/src/main/java/com/banoka/MainActivity.java
+++ android/app/src/main/java/com/banoka/MainActivity.java
@@ -1,4 +1,4 @@
-package com.calculators;
+package com.banoka;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "Calculators";
+        return "Banoka";
     }
 }
