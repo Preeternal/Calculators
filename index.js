945b0949a60be043d7604b4b09a8f6e5b27b567f
Preeternal <preeternal@ya.ru>
2019-02-18 20:01:36 +0200

    detect system language change

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 import { AppRegistry } from 'react-native';
+import RNLanguages from 'react-native-languages';
+import i18n from 'i18n-js';
 
 import store from './src/store';
 import App from './src/screens';
 import { name as appName } from './app';
 
-const Start = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+class Start extends PureComponent {
+  componentDidMount() {
+    RNLanguages.addEventListener('change', this.handleLanguageChange);
+  }
+
+  componentWillUnmount() {
+    RNLanguages.removeEventListener('change', this.handleLanguageChange);
+  }
+
+  handleLanguageChange = ({ language }) => {
+    i18n.locale = language;
+  };
+
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
 
 AppRegistry.registerComponent(appName, () => Start);
