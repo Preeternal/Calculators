98736e35f043ea60a380c542ed07ba0b3aa6af9f
Preeternal <preeternal@ya.ru>
2019-06-11 12:16:19 +0300

    upgrade apollo-boost

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
-import ApolloBoost from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 
-const client = new ApolloBoost({
+const client = new ApolloClient({
   uri: 'https://aqueous-reef-98968.herokuapp.com/',
   // uri: 'http://127.0.0.1:4000',
 });
