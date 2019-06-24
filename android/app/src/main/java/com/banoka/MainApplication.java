a1eb328a783b07d889fc68adf9e397846cb639a8
Preeternal <preeternal@ya.ru>
2019-06-24 14:59:46 +0300

    add lottie-react-native@2.5.11

--- android/app/src/main/java/com/banoka/MainApplication.java
+++ android/app/src/main/java/com/banoka/MainApplication.java
@@ -3,6 +3,7 @@ package com.banoka;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new LottiePackage(),
             new RNGestureHandlerPackage(),
             new RNIapPackage(), new VectorIconsPackage(), new RNLanguagesPackage());
     }
