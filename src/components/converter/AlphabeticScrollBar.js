ffb90c65b4d74f1ebbc62e14a382aff86a063965
Preeternal <preeternal@ya.ru>
2020-03-03 12:14:45 +0200

    temp

--- src/components/converter/AlphabeticScrollBar.js
+++ src/components/converter/AlphabeticScrollBar.js
@@ -2,27 +2,45 @@ import React from 'react';
 import { View, Text, TouchableWithoutFeedback } from 'react-native';
 
 const AlphabeticScrollBar = props => {
+  const onTouchEvent = (name, ev) => {
+    console.log(
+      `[${name}] ` +
+        `root_x: ${ev.nativeEvent.pageX}, root_y: ${ev.nativeEvent.pageY} ` +
+        `target_x: ${ev.nativeEvent.locationX}, target_y: ${ev.nativeEvent.locationY} ` +
+        `target: ${ev.nativeEvent.target}`,
+    );
+  };
   return (
-    <View style={[styles.container, props.scrollBarContainerStyle]}>
+    <View
+      onStartShouldSetResponder={ev => true}
+      // onMoveShouldSetResponder={(ev) => false}
+      onResponderGrant={e => onTouchEvent('onResponderGrant', e)}
+      // onResponderReject={this.onTouchEvent.bind(this, "onResponderReject")}
+      onResponderMove={e => onTouchEvent('onResponderMove', e)}
+      // onResponderRelease={this.onTouchEvent.bind(this, "onResponderRelease")}
+      // onResponderTerminationRequest={(ev) => true}
+      // onResponderTerminate={this.onTouchEvent.bind(this, "onResponderTerminate")}
+      style={[styles.container, props.scrollBarContainerStyle]}
+    >
       {props.alphabet.map(letter => (
-        <TouchableWithoutFeedback
-          key={letter}
-          acceptsKeyboardFocus
-          onFocus={() => props.scrollToIndex(letter)}
-          onPressIn={() => props.scrollToIndex(letter)}
-        >
-          <View key={letter}>
-            <Text
-              style={{
-                color: props.viewableLetters.includes(letter)
-                  ? 'rgba(231, 76, 60, 1)'
-                  : 'gray',
-              }}
-            >
-              {letter}
-            </Text>
-          </View>
-        </TouchableWithoutFeedback>
+        // <TouchableWithoutFeedback
+        //   key={letter}
+        //   acceptsKeyboardFocus
+        //   onFocus={() => props.scrollToIndex(letter)}
+        //   onPressIn={() => props.scrollToIndex(letter)}
+        // >
+        <View onFocus={() => props.scrollToIndex(letter)} key={letter}>
+          <Text
+            style={{
+              color: props.viewableLetters.includes(letter)
+                ? 'rgba(231, 76, 60, 1)'
+                : 'gray',
+            }}
+          >
+            {letter}
+          </Text>
+        </View>
+        // </TouchableWithoutFeedback>
       ))}
     </View>
   );
